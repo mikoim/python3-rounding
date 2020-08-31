188410e2d8f142b8bbf611f17f0bd5e7c0a7a060
Eshin Kunishima <ek@esh.ink>
2020-08-31 22:39:35 +0900

    Use unittest instead of nose

--- tests/rounding_tests.py
+++ tests/rounding_tests.py
@@ -1,24 +1,20 @@
 __author__ = 'dmaust'
 
-from nose.tools import *
-import rounding
 import random
+import unittest
 
-def setup():
-    pass
-
-def teardown():
-    pass
-
-def test_standard():
-    standard = rounding.StandardRound()
-    assert standard.round(123.45)==123, "Rounds down normally"
-
-def test_stochastic():
-    stochastic = rounding.StochasticRound(random_generator=random.Random(123))
-    total = sum(stochastic.round(123.45) for i in range(100))
-    assert (12340 <= total <= 12360), "Rounds down some of the time:%d" % (total,)
-    # error + total = expected
-    eq_(total-stochastic.cumulative_error, 12345)
+import rounding
 
 
+class TestRounding(unittest.TestCase):
+
+    def test_standard(self):
+        standard = rounding.StandardRound()
+        assert standard.round(123.45) == 123, "Rounds down normally"
+
+    def test_stochastic(self):
+        stochastic = rounding.StochasticRound(random_generator=random.Random(123))
+        total = sum(stochastic.round(123.45) for i in range(100))
+        assert (12340 <= total <= 12360), "Rounds down some of the time:%d" % (total,)
+        # error + total = expected
+        self.assertEqual(total - stochastic.cumulative_error, 12345)
